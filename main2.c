553412cf25d85fbd7715c13daa036a5d0deed1b4
404-eli <ellenfaithlamzon@gmail.com>
2023-12-29 20:39:42 +0800

    Update main2.c

--- main2.c
+++ main2.c
@@ -116,7 +116,7 @@ void adminPanel(){
 				searchrecords();
                 break;
             case 'B':   
-                // addBill();
+                addBill();
                 break;
 			case 'D':
 				deleterecords();
@@ -223,7 +223,7 @@ void listrecords() {
         printf("Error opening the file.\n");
         return;
     }
-// 
+
     system("cls");
     printf("\n%-15s%-15s%-15s%-18s%-10s%-15s%-15s\n", "Username", "Room Number", "Room Rate", "Num of Tenants", "Bill", "Days to Pay", "Total Payment");
     for (i = 0; i < 95; i++) {
@@ -478,6 +478,7 @@ void payment(){
     fclose(file);
 }
 
+<<<<<<< Updated upstream
 //DELETING RECORDS---------------------------------------------------------------------------------------------------------
 
 
@@ -585,11 +586,92 @@ void deleterecords() {
 
     if (counter >= 5) {
         printf("Maximum attempts reached!\n");
+=======
+void addBill(){
+    FILE *file = fopen("tenant_records.txt", "r");
+
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return;
+    }
+
+    char username_search[256];
+
+    // Get input for search
+    system("cls");
+    printf("\nEnter username: ");
+    fgets(username_search, sizeof(username_search), stdin);
+    username_search[strcspn(username_search, "\n")] = '\0';  // Remove the newline character
+
+    // Skip the first line (header)
+    char line[256];
+    if (fgets(line, sizeof(line), file) == NULL) {
+        printf("Error reading the header.\n");
+        fclose(file);
+        return;
+    }
+
+    // Search for the record
+    int record_found = 0;
+    FILE *tempFile = fopen("temp_records.txt", "w");
+
+    fprintf(tempFile, "%s", line);  // Write the header to the temp file
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char *record_username = strtok(line, ",");
+        char *room_number = strtok(NULL, ",");
+        char *room_rate = strtok(NULL, ",");
+        char *num_of_tenants = strtok(NULL, ",");
+        char *electricity = strtok(NULL, ",");
+        char *water = strtok(NULL, ",");
+        char *days_to_pay = strtok(NULL, ",");
+        float total_payment = atof(strtok(NULL, ","));  // Retrieve total_payment as a float
+
+        // Trim whitespaces from the found record values
+        trim_whitespace(record_username);
+
+        // Compare with the provided username
+        if (strcmp(record_username, username_search) == 0) {
+            // Display the found record along with total_payment
+            printf("%-15s%-15s%-15s%-18s%-10s%-10s%-20s\n", "Username", "Room Number", "Room Rate", "Num of Tenants", "Bill", "Days to Pay", "Total Payment");
+
+            float bill = atof(electricity) + atof(water);
+            printf("%-15s%-15s%-15s%-18s%-10.2f%-15s%-20.2f\n", record_username, room_number, room_rate, num_of_tenants, bill, days_to_pay, total_payment);
+            record_found = 1;
+
+            // Get new electricity and water bill values from the user
+            float new_electricity, new_water;
+            printf("\nEnter new electricity bill: ");
+            scanf("%f", &new_electricity);
+            printf("Enter new water bill: ");
+            scanf("%f", &new_water);
+
+            float new_bill = new_electricity + new_water;
+            float total_payment = new_bill + (atoi(room_rate) * atoi(days_to_pay));
+            // Append the modified record to the temp file
+            fprintf(tempFile, "%s,%s,%s,%s,%.2f,%.2f,%s,%.2f\n", record_username, room_number, room_rate, num_of_tenants, new_electricity, new_water, days_to_pay, total_payment);
+        } else {
+            // Write the unchanged record to the temp file
+            fprintf(tempFile, "%s,%s,%s,%s,%s,%s,%s,%.2f\n", record_username, room_number, room_rate, num_of_tenants, electricity, water, days_to_pay, total_payment);
+        }
+    }
+
+    fclose(file);
+    fclose(tempFile);
+
+    if (!record_found) {
+        printf("Record not found.\n");
+        remove("temp_records.txt");  // Remove the temporary file if record not found
+    }
+    else{
+    // Remove the original file and rename the temporary file to the original file
+    remove("tenant_records.txt");
+    rename("temp_records.txt", "tenant_records.txt");
+>>>>>>> Stashed changes
     }
 }
 
 //additional functions
-
 void writeHeader(FILE *record, const char *header) {
     fprintf(record, "%s\n", header);
 }
@@ -613,3 +695,12 @@ void trim_whitespace(char *str) {
 
     str[i - start] = '\0';
 }
+<<<<<<< Updated upstream
+=======
+
+
+//Analysis: remove water bill and electricity bill merge it to total payment column
+//Do not display how many tenants in each room
+//
+//Bugs: cannot modify data
+>>>>>>> Stashed changes
